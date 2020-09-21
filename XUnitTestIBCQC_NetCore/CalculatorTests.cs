d70a63384994198ca3cb17f9f599b94f8915b745
DaveJW <david.worrall@proximitum.com>
2020-09-21 16:07:19 +0100

    Added Unit Test examples
    Add a new class in the core app to illustrate how to use xUnit for testing. New cotroller added workign on a refresh for the key delivery

---
+++ XUnitTestIBCQC_NetCore/CalculatorTests.cs
@@ -0,0 +1,49 @@
+using IBCQC_NetCore.Functions;
+using System;
+using Xunit;
+
+namespace XUnitTestIBCQC_NetCore
+{
+    public class CalculatorTests
+    {
+
+        //very basic just add two numbers and test
+        [Fact]
+        public void AddTwoNumbers()
+        {
+            int a = 1;
+            int b = 2;
+
+            //sut  is Subject under Test
+            var sut = new Calculator();
+
+            //send test data
+            var result = sut.Add(a, b);
+
+            //see if we got what we expected
+            //Assert.Equal(expected,actual returned)
+
+            Assert.Equal(3, result);
+
+        }
+
+        ///example of data driven test  
+
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-4, -6, -10)]
+        [InlineData(-2, 2, 0)]
+        public void Can_Add_Two_Numbers_Data_Driven(int a, int b, int expectedResult)
+        {
+            // Arrange
+            var sut = new Calculator();
+
+            // Act
+            var actualResult = sut.Add(a, b);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+    }
+    }
+}
