78aa054daa0b250d2c5fc3e30e23454372150444
DaveJW <david.worrall@proximitum.com>
2020-09-29 14:00:56 +0100

    Login updated to accept blank post request
    Backward compatability with old API

--- IBCQC_NetCore/Controllers/LoginController.cs
+++ IBCQC_NetCore/Controllers/LoginController.cs
@@ -137,41 +137,42 @@ namespace IBCQC_NetCore.Controllers
 
         [HttpPost]
 
-        public IActionResult Login([FromBody] string posted)
+        public IActionResult Login( string posted)
         {
             try
             {
-                // Certificate request
-
-                //As the requirement for client cert is baked into server and also through DI 
-                //see program.cs for host builder and  Startup.cs for the certificate fail and success 
-                //actions Success will populate the claimsprincipal in the controller base
-
-                //todo extend the activation to turn off certs if required for testing
-
-                //this will retun a forbidden if no vaid cert is found this can b extended to only allow registered cluent certificate sif required
-
-                ClaimsPrincipal currentUser = this.User;
-                //get the claims from the certificate context
-
-                //we have dual claims at this moment defind in startuop.cs
 
                 //cert Serial Number
 
-                string certSerial = currentUser.Claims.FirstOrDefault(
-             c => c.Type == ClaimTypes.SerialNumber)?.Value;
+
+                var cert = Request.HttpContext.Connection.ClientCertificate;
+
+                // Get the public key
+                byte[] userPublicKey = cert.GetPublicKey();
+
+                string certSerial = cert.SerialNumber;
+
+
+                if (certSerial.Length < 18)
+                {
+                    certSerial = certSerial.PadLeft(18, '0');
+                }
+
+
+
+           
                 if (certSerial == null)
                 {
                     return Unauthorized("No Serial Number retrieved from Certificate");
                 }
 
                 //Friendly Certificate Name 
-                string certFriendlyName = currentUser.Claims.FirstOrDefault(
-             c => c.Type == ClaimTypes.Name)?.Value;
+                string certFriendlyName = cert.FriendlyName; 
                 if (certFriendlyName == null)
                 {
                     return Unauthorized("No Friendly Name Associated with this certificate");
                 }
+
 
 
                 JwtTokenHandler getToken = new JwtTokenHandler();
