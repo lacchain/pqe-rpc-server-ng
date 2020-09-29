ff44e1bdf345a56df2a784575c42c78b96277b00
DaveJW <david.worrall@proximitum.com>
2020-09-29 13:33:33 +0100

    fix cert error oncklaims principal
    retrieve certificate directly form callgn context

--- IBCQC_NetCore/Controllers/LoginController.cs
+++ IBCQC_NetCore/Controllers/LoginController.cs
@@ -67,19 +67,33 @@ namespace IBCQC_NetCore.Controllers
                 //get the claims from the certificate context
 
                 //we have dual claims at this moment defind in startuop.cs
-             
+
                 //cert Serial Number
 
-                string certSerial = currentUser.Claims.FirstOrDefault(
-             c => c.Type == ClaimTypes.SerialNumber)?.Value;
+
+                var cert = Request.HttpContext.Connection.ClientCertificate;
+
+                // Get the public key
+                byte[] userPublicKey = cert.GetPublicKey();
+
+              string  certSerial = cert.SerialNumber;
+
+
+                if (certSerial.Length < 18)
+                {
+                    certSerial = certSerial.PadLeft(18, '0');
+                }
+
+
+
+              //  string certSerial = currentUser.Claims.FirstOrDefault(c => c.Type == ClaimTypes.SerialNumber)?.Value;
                 if (certSerial == null)
                 {
                     return Unauthorized("No Serial Number retrieved from Certificate");
                 }
 
                 //Friendly Certificate Name 
-                string certFriendlyName = currentUser.Claims.FirstOrDefault(
-             c => c.Type == ClaimTypes.Name)?.Value;
+                string certFriendlyName = cert.FriendlyName;   //currentUser.Claims.FirstOrDefault( c => c.Type == ClaimTypes.Name)?.Value;
                 if (certFriendlyName == null)
                 {
                     return Unauthorized( "No Friendly Name Associated with this certificate");
@@ -127,6 +141,93 @@ namespace IBCQC_NetCore.Controllers
             {
                 // APILogging.Log("Login", "ERROR: Login failed with exception: " + ex.Message);
                 return Unauthorized( "*401* Login failed. ");
+            }
+        }
+
+
+
+
+
+        [HttpPost]
+
+        public IActionResult Login([FromBody] string posted)
+        {
+            try
+            {
+                // Certificate request
+
+                //As the requirement for client cert is baked into server and also through DI 
+                //see program.cs for host builder and  Startup.cs for the certificate fail and success 
+                //actions Success will populate the claimsprincipal in the controller base
+
+                //todo extend the activation to turn off certs if required for testing
+
+                //this will retun a forbidden if no vaid cert is found this can b extended to only allow registered cluent certificate sif required
+
+                ClaimsPrincipal currentUser = this.User;
+                //get the claims from the certificate context
+
+                //we have dual claims at this moment defind in startuop.cs
+
+                //cert Serial Number
+
+                string certSerial = currentUser.Claims.FirstOrDefault(
+             c => c.Type == ClaimTypes.SerialNumber)?.Value;
+                if (certSerial == null)
+                {
+                    return Unauthorized("No Serial Number retrieved from Certificate");
+                }
+
+                //Friendly Certificate Name 
+                string certFriendlyName = currentUser.Claims.FirstOrDefault(
+             c => c.Type == ClaimTypes.Name)?.Value;
+                if (certFriendlyName == null)
+                {
+                    return Unauthorized("No Friendly Name Associated with this certificate");
+                }
+
+
+                JwtTokenHandler getToken = new JwtTokenHandler();
+
+
+                var tokenExpiry = Startup.StaticConfig["Config:jwt_lifetime"];
+                var tokenSecret = Startup.StaticConfig["Config:jwt_secret"];
+                var tokenIssuer = Startup.StaticConfig["Config:jwt_issuer"];
+                var tokenAudience = Startup.StaticConfig["Config:jwt_audience"];
+
+                var jwtTok = getToken.GenerateToken(certSerial, tokenExpiry, tokenSecret, tokenIssuer, tokenAudience);
+
+
+
+
+
+                //send the usual token format
+
+                JwtTokenResult issueToken = new JwtTokenResult()
+                {
+                    Token = jwtTok,
+                    NotBefore = DateTime.UtcNow,
+                    NotAfter = DateTime.UtcNow.AddHours(7)
+
+                };
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    WriteIndented = true
+                };
+
+
+
+
+                var userJwtTok = JsonSerializer.Serialize(issueToken, options);
+
+                return Ok(userJwtTok);
+            }
+            catch (Exception ex)
+            {
+                // APILogging.Log("Login", "ERROR: Login failed with exception: " + ex.Message);
+                return Unauthorized("*401* Login failed. ");
             }
         }
 
