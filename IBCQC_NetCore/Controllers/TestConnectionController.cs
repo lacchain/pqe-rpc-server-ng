56a1ca9715dffe703f931056557ed1b5f47ddad1
DaveJW <david.worrall@proximitum.com>
2020-10-02 12:51:25 +0100

    Create TestConnectionController.cs

---
+++ IBCQC_NetCore/Controllers/TestConnectionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IBCQC_NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestConnectionController : ControllerBase
+    {
+        // GET: api/<TestConnectionController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return StatusCode(200, "Alive");
+        }
+
+       
+    }
+}
