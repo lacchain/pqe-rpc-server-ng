8c6da51bc9f216683c1f3dc65669a291aaf914c0
DaveJW <david.worrall@proximitum.com>
2020-10-28 14:39:35 +0000

    get info controller tested
    url https://localhost:32776/api/getinfo
    
    output
    
    {
    
        "major": 2020,
        "minor": 9,
        "build": 30,
        "revision": 1,
        "variant": " (CSMP:NoKEM)",
        "versionStr": "2020.09.30.01 (CSMP:NoKEM)",
        "buildDate": "2020-10-28T14:36:56+00:00",
        "configuration": "Release",
        "targetFramework": ".NETCoreApp,Version=v3.1",
        "supportedEndpoints": "getinfo|api/getinfo|api/login|api/reqkeypair|api/setupclient|api/sharedsecret|api/srng|api/verifykeys"
    }

--- IBCQC_NetCore/Program.cs
+++ IBCQC_NetCore/Program.cs
@@ -24,55 +24,13 @@ namespace IBCQC_NetCore
             // Have option to remove certificate auth if we want, but need the serial number
             // so for testing that would need injecting
 
-            bool certificateRequired = true;
-
-            if (certificateRequired)
-            {
+        
+          
                 CreateHostBuilder(args).Build().Run();
-            }
-            else
-            {
-                CreateHostBuilderNonSecure(args).Build().Run();
-            }
+          
         }
 
 
-        public static IHostBuilder CreateHostBuilderNonSecure(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-             .ConfigureServices((context, services) =>
-             {
-                 services.Configure<KestrelServerOptions>(
-                     context.Configuration.GetSection("Config:Kestrel"));
-             })
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    logging.ClearProviders();
-                   
-                    logging.AddConsole(options => options.IncludeScopes = true);
-
-                    // Debug Logging Provider:
-                    // The Debug provider writes log output by using the System.Diagnostics.Debug class.
-                    // Calls to System.Diagnostics.Debug.WriteLine write to the Debug provider.
-                    // On Linux, the Debug provider log location is distribution-dependent
-                    // and may be one of the following:
-                    //   /var/log/message
-                    //   /var/log/syslog
-                    //logging.AddDebug();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-
-                    webBuilder.ConfigureKestrel(o =>
-                    {
-                        o.ConfigureHttpsDefaults(o =>
-                            //o.ClientCertificateMode =  ClientCertificateMode.RequireCertificate
-                            o.ClientCertificateMode =  ClientCertificateMode.NoCertificate
-                       );
-                    });
-                });
-        }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
