096a97e99534319799981798f71c7f3e8d31f872
DaveJW <david.worrall@proximitum.com>
2020-11-19 15:09:02 +0000

    Update NewClientData.cs

--- IBCQC_NetCore/Models/NewClientData.cs
+++ IBCQC_NetCore/Models/NewClientData.cs
@@ -5,7 +5,23 @@ using System.Threading.Tasks;
 
 namespace IBCQC_NetCore.Models
 {
-    public class NewClientExt
+
+
+
+    public class Channel
     {
+        public string type { get; set; }
+        public string value { get; set; }
     }
+
+    public class NewClientData
+    {
+        public string clientCertName { get; set; }
+        public string clientCertSerialNumber { get; set; }
+        public string countryCode { get; set; }
+        public List<Channel> channels { get; set; }
+        public string kemAlgorithm { get; set; }
+    }
+
+
 }
